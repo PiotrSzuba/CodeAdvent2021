5defab6b0ad3f373577946234af628c8f62391ed
Piotr Szuba <sakonir@gmail.com>
2021-12-07 13:12:14 +0100

    Added day 7

---
+++ CodeAdvent/Day7.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent2021
+{
+    public class Day7
+    {
+        private List<int> _data { get; set; } = new();
+        public Day7(List<string> data)
+        {
+            _data = data[0].Split(",").Select(x => int.Parse(x)).ToList();
+        }
+
+        private int GetAvg() => _data.Sum() / _data.Count;
+
+        private long GetCostA(int moveTo)
+        {
+            List<int> data = _data;
+            int sum = 0;
+            data.ForEach(x => sum += Math.Abs(x - moveTo));
+
+            return Convert.ToInt64(sum);
+        }
+
+        double GetDistance(long x) => ((x * x) * 0.5) + x * 0.5;
+
+        private long GetCostB(int moveTo)
+        {
+            List<int> data = _data;
+            double sum = 0;
+            data.ForEach(x => sum += GetDistance(Math.Abs(x - moveTo)));
+
+            return Convert.ToInt64(sum);
+        }
+        private long Solve(Func<int,long> func)
+        {
+            long upperBound = func(_data.Count / 2);
+            int lowerBound = GetAvg() / 2;
+            long bestCost = int.MaxValue;
+            long currentCost = 0;
+            long prevCost = 0;
+
+            for (int i = lowerBound; i < upperBound; i++)
+            {
+                if (i > 0)
+                {
+                    prevCost = func(i - 1);
+                }
+                currentCost = func(i);
+                if (currentCost > prevCost)
+                {
+                    break;
+                }
+                if (bestCost > currentCost)
+                {
+                    bestCost = currentCost;
+                }
+            }
+
+            return bestCost;
+        }
+
+        public long PartA() => Solve(GetCostA);
+        public long PartB() => Solve(GetCostB);
+    }
+    
+}
--- CodeAdvent/Program.cs
+++ CodeAdvent/Program.cs
@@ -2,23 +2,26 @@
 using System;
 //https://adventofcode.com/
 
-Day1 day1 = new(ReadFromFile.Read(@"DataDay1A.txt"));
+Day1 day1 = new(ReadFromFile.Read(@"Day1A.txt"));
 Console.WriteLine("Day1 A: {0} B: {1}", day1.PartA(), day1.PartB());
 
-Day2 day2 = new (ReadFromFile.Read(@"DataDay2A.txt"));
+Day2 day2 = new (ReadFromFile.Read(@"Day2A.txt"));
 Console.WriteLine("Day2 A: {0} B: {1}", day2.PartA(), day2.PartB());
 
-Day3 day3 = new(ReadFromFile.Read(@"DataDay3B.txt"));
+Day3 day3 = new(ReadFromFile.Read(@"Day3B.txt"));
 Console.WriteLine("Day3 A: {0} B: {1}", day3.PartA(), day3.PartB());
 
-Day4 day4 = new(ReadFromFile.Read(@"DataDay4B.txt"));
+Day4 day4 = new(ReadFromFile.Read(@"Day4B.txt"));
 Console.WriteLine("Day4 A: {0} B: {1}", day4.PartA(), day4.PartB());
 
-Day5 day5 = new(ReadFromFile.Read(@"DataDay5B.txt"));
+Day5 day5 = new(ReadFromFile.Read(@"Day5B.txt"));
 Console.WriteLine("Day5 A: {0} B: {1}", day5.PartA(), day5.PartB());
 
-Day6 day6 = new(ReadFromFile.Read(@"DataDay6B.txt"));
+Day6 day6 = new(ReadFromFile.Read(@"Day6B.txt"));
 Console.WriteLine("Day6 A: {0} B: {1}", day6.PartA(), day6.PartB());
+
+Day7 day7 = new(ReadFromFile.Read(@"Day7B.txt"));
+Console.WriteLine("Day7 A: {0} B: {1}", day7.PartA(), day7.PartB());
 
 Console.WriteLine("\nPress anything to exit");
 Console.ReadKey();
